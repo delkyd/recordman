4547f28032053d219718438d4b3cfb76d2819544
liu yuhua <liuyuhua@outlook.com>
2015-11-08 23:32:35 +0800

    代码框架

---
+++ WebContent/resources/pages/include/html_head.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>录波侠-故障录波器管理平台</title>
+	<script type="text/javascript">
+		var rootPath = "${rootPath}";
+	</script>
+	<link rel="stylesheet" href="<%=request.getContextPath()%>/resources/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="<%=request.getContextPath()%>/resources/css/style.css" />
+	
---
+++ src/recordman/mvc/LoginController.java
@@ -0,0 +1,46 @@
+package recordman.mvc;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/logon")
+public class LoginController {
+	
+	public LoginController(){
+		
+	}
+	
+	@Inject
+	//private UserDataHandle userdatahandle;
+	
+	@RequestMapping(value="/login")
+	public String login(@RequestParam("name") String userName,
+						@RequestParam("pwd") String userPwd,Model model, HttpServletRequest request){
+		try{						
+			System.out.println("login method");
+			
+			String rootpath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()+request.getContextPath();
+			model.addAttribute("rootPath", rootpath);
+			request.getSession().setAttribute("rootPath", rootpath );
+			if( true ){
+				System.out.println("login successed");
+				return "redirect:/mgn/leavehospital/showinhospitals";
+			}else{
+				System.out.println("login failed");
+				model.addAttribute("loginFail", true);
+				return "login";
+			}
+			
+		}catch( Exception e ){
+			e.printStackTrace();			
+			return "login";
+		}
+	}
+
+}
