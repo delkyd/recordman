f0045bcc8158931b0b66aa14917412364439308b
liuyuhua <liuyuhua@outlook.com>
2016-03-25 20:38:55 +0800

    日志读取

---
+++ src/recordman/datahandle/LogDataHandle.java
@@ -0,0 +1,52 @@
+package recordman.datahandle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.bson.Document;
+
+import codeman.util.DTF;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCursor;
+
+import recordman.bean.logmsg;
+
+public class LogDataHandle {
+	// 输出日志文件
+	private static Logger logger = Logger.getLogger(LogDataHandle.class);
+	public List<logmsg> findLogs( String level ){
+		try{
+			MongoDao dao = MongoDao.GetInstance();
+
+			BasicDBObject cond = new BasicDBObject();
+			if( null != level && !level.isEmpty() ){
+				cond.append("level", new BasicDBObject("$eq", level));
+			}
+			List<logmsg> list = new ArrayList<logmsg>();
+			FindIterable<Document> result = dao.GetCollection("sys_logs").find(cond);
+			BasicDBObject sort = new BasicDBObject();
+			sort.append("date", -1);
+			result.sort(sort);
+			MongoCursor<Document> it = result.iterator();
+			while( it.hasNext() ){
+				Document doc = it.next();
+				logmsg r = new logmsg();
+				r.setId(doc.getLong("id"));
+				r.setContent( doc.getString("content"));
+				r.setDate( DTF.DateToString( doc.getDate("date"), "yyyy-MM-dd HH:mm:ss.SSS"));
+				r.setIpaddr( doc.getString("ipaddr"));
+				r.setLevel( doc.getString("level"));
+				r.setUser( doc.getString("user"));
+				list.add(r);
+			}
+			return list;
+		}catch(Exception e){
+			e.printStackTrace();
+			logger.error(e.toString());
+			return null;
+		}	
+	}
+}
